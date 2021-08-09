8f756de32a55647a771087751a4059930f244c92
codetyphon <codetyphon@outlook.com>
2021-08-09 16:20:39 +0800

    lession3

--- main.lua
+++ main.lua
@@ -1,11 +1,23 @@
 function love.load()
   x = 0
+  y = 0
 end
 
 function love.update(dt)
-  x = x + 100 * dt
+  if love.keyboard.isDown("d") then
+    x = x + 100 * dt
+  end
+  if love.keyboard.isDown("a") then
+    x = x - 100 * dt
+  end
+  if love.keyboard.isDown("w") then
+    y = y - 100 * dt
+  end
+  if love.keyboard.isDown("s") then
+    y = y + 100 * dt
+  end
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", x,10,30,30)
+    love.graphics.rectangle("fill", x, y, 30, 30)
 end
