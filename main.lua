e42051d5a845dea65c5db3ca13b1f9b9fa3dbab5
codetyphon <codetyphon@outlook.com>
2021-08-09 16:15:10 +0800

    lession2

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
 function love.load()
-
+  x = 0
 end
 
 function love.update(dt)
-
+  x = x + 100 * dt
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", 10,10,30,30)
+    love.graphics.rectangle("fill", x,10,30,30)
 end
