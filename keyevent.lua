9e5ec48f291d7184a525faa9395c240ae0539d5b
codetyphon <codetyphon@outlook.com>
2021-08-10 15:20:49 +0800

    lesson6

--- keyevent.lua
+++ keyevent.lua
@@ -1,5 +1,5 @@
 --! file: keyevent.lua
-function key_event(player,dt)
+function key_event(player,dt,make_a_projectile)
   if love.keyboard.isDown("d") then
     player.x = player.x + player.speed * dt
   end
@@ -11,6 +11,9 @@ function key_event(player,dt)
   end
   if love.keyboard.isDown("s") then
     player.y = player.y + player.speed * dt
+  end
+  if love.keyboard.isDown("space") then
+    make_a_projectile()
   end
 end
 
--- main.lua
+++ main.lua
@@ -1,18 +1,88 @@
 local key_event = require "keyevent"
 local player = {}
+local enemys = {}
+local foods = {}
+local projectiles={}
+local time = 0
+
+
+function make_a_enemy()
+  local enemy = {}
+  enemy.x = math.random(100, 500)
+  enemy.y = 0
+  enemy.width = 50
+  enemy.height = 50
+  table.insert(enemys, enemy)
+end
+
+function make_a_food()
+  local food = {}
+  food.x = math.random(100, 500)
+  food.y = 0
+  food.width = 50
+  food.height = 50
+  table.insert(foods, food)
+end
+
+function make_a_projectile()
+  local projectile = {}
+  projectile.x = player.x + player.width/2 - 5
+  projectile.y = player.y
+  projectile.width = 10
+  projectile.height = 10
+  projectile.speed = 300
+  table.insert(projectiles, projectile)
+end
 
 function love.load()
-  player.x = 0
-  player.y = 0
-  player.weight = 30
+  player.x = 500/2 - 15
+  player.y = 500 - 30
+  player.width = 30
   player.height = 30
   player.speed = 100
 end
 
 function love.update(dt)
-  key_event(player, dt)
+  time = time + 1
+  
+  if time % 100 == 0 then
+    make_a_enemy()
+  end
+  if time % 300 == 0 then
+    make_a_food()
+  end
+  
+  key_event(player, dt ,make_a_projectile)
+  for i,v in ipairs(enemys) do
+    print(i, v)
+    v.y = v.y + 100 * dt
+  end
+  for i,v in ipairs(foods) do
+    print(i, v)
+    v.y = v.y + 100 * dt
+  end
+  for i,v in ipairs(projectiles) do
+    print(i, v)
+    v.y = v.y - v.speed * dt
+  end
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", player.x, player.y, player.weight, player.height)
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+  for i,v in ipairs(enemys) do
+    print(i, v)
+    love.graphics.setColor(255, 0, 0)
+    love.graphics.rectangle("fill", v.x, v.y, v.width, v.height)
+  end
+  for i,v in ipairs(foods) do
+    print(i, v)
+    love.graphics.setColor(50, 200, 50)
+    love.graphics.rectangle("fill", v.x, v.y, v.width, v.height)
+  end
+  for i,v in ipairs(projectiles) do
+    print(i, v)
+    love.graphics.setColor(50, 200, 50)
+    love.graphics.rectangle("fill", v.x, v.y, v.width, v.height)
+  end
 end
